84b4e12b8652f4a3c9315176700d50b7bd7afe56
Ishaan Rajan <ishaanrajan37@gmail.com>
2025-04-30 18:42:52 -0700

    end to end user auth flow

--- kalos/src/pages/signup.js
+++ kalos/src/pages/signup.js
@@ -5,17 +5,39 @@ import { useNavigate } from 'react-router-dom';
 export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
   const navigate = useNavigate();
 
   async function handleSignup() {
-    const { error } = await supabase.auth.signUp({ email, password });
-    if (error) console.error(error);
-    else navigate('/');
+    const { data, error } = await supabase.auth.signUp({ email, password });
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    const user = data.user;
+    console.log("User:", user);
+    const { error: insertError } = await supabase
+        .from('users')
+        .insert([
+            {id: user.id, email: user.email, username: username, profile_pic: null}
+        ]);
+    if (insertError) {
+        console.error(insertError);
+    } else {
+        navigate('/');
+    }
   }
 
   return (
     <div>
       <h1>🆕 Signup</h1>
+      <input
+        type="username"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
       <input
         type="email"
         placeholder="Email"
--- kalos/src/supabaseClient.js
+++ kalos/src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://sxhilxrmifrmdxwgfdxx.supabase.co"
-const supabaseKey = "dOaTXrq//YvnNICGUmszY7G3D++cMGrWss5H0hlhhx2Gyz0UI9wgJV9vuthLl35h+DeG/9abBytp0t7R9iyqyg=="
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4aGlseHJtaWZybWR4d2dmZHh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2ODMwMzcsImV4cCI6MjA2MDI1OTAzN30.StBiuy_ELe07tYpIQeRtdkJRbpsdX7Y8nlLxLyyhGX0"
+                    
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
